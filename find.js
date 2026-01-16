f1d4b613bd53e609f6e6ea6c0c4f0a2a67cd2af4
arshie26 <arshtist@gmail.com>
2026-01-16 15:16:31 -0500

    Home and Mobile versions

--- find.js
+++ find.js
@@ -1,4 +1,8 @@
-
+if(document.querySelector(".cards")){
+    console.log("new page");
+    let moviesList = JSON.parse(localStorage.getItem("movies"));
+    renderMovies(moviesList);
+}
 
 function openMenu(){
     document.body.classList += "open__menu";
@@ -9,13 +13,23 @@ function closeMenu(){
 }
 
 async function getMovies(event){
+    let cardsWrapper = document.querySelector(".cards");
+    cardsWrapper.innerHTML = `
+        <div class="spinner__container">
+            <i class="fas fa-spinner movies__loading--spinner"></i>
+        </div>`;
     let userSearch = event.target.value;
     let moviesPromise = await fetch(`https://www.omdbapi.com/?s=${userSearch}&apikey=b971c236`);
     let moviesList = await moviesPromise.json();
     console.log(moviesList.Search);
-    setTimeout(() => {
+    renderMovies(moviesList);
+} 
+
+function renderMovies(moviesList){
+    setTimeout(() => {    
         let cardsWrapper = document.querySelector(".cards");
         cardsWrapper.innerHTML = moviesList.Search.map((movie) => {
+        
             return `
             <div class="card">
                 <div class="img__container">
@@ -38,6 +52,17 @@ async function getMovies(event){
                 </div>
             </div>`
         }).join("");
-    });
-    
+    })
+}
+
+async function searchFromHome(event){
+    let query = document.querySelector(".home__search");
+    let buttonWrapper = document.querySelector(".search__button");
+    buttonWrapper.innerHTML = `<i class="fas fa-spinner movies__loading--spinner"></i>`;
+    console.log("Search term was ", query.value);
+    let moviesPromise = await fetch(`https://www.omdbapi.com/?s=${query.value}&apikey=b971c236`);
+    let moviesList = await moviesPromise.json();
+    console.log(moviesList);
+    localStorage.setItem("movies", JSON.stringify(moviesList));
+    window.location.href="http://127.0.0.1:8080/FES-M4-Final/find.html";
 }
